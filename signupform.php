415e30fa37341df978002fd0173d656f249abc6f
nayan-pust <114688354+nayan-pust@users.noreply.github.com>
2023-08-15 01:34:29 +0600

    Add files via upload

---
+++ signupform.php
@@ -0,0 +1,44 @@
+<doctype html>
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://fontawesome.com/icons/lock?f=classic&s=solid">
+            <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+            <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    
+
+            <title>Registration form fill</title>
+            
+        </head>
+        <body>
+            <div id="form" align="center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-3">
+                        <form align="center">
+                    <h1 name="signup">SignUp Form</h1>
+                    <hr class="mb-3">
+                <i class="fa-sharp fa-solid fa-user"></i>
+                    <input type="text" name="user" id="user"placeholder="Enter the user name"><br>
+                    <hr class="mb-3">
+                    <i class="fa-solid fa-envelope"></i>
+                    <input type="email" name="email" id="email"placeholder="Email"><br>
+                    <hr class="mb-3">
+                    <i class="fa-solid fa-lock"></i>
+                    <input type="password" name="pass" id="pass"placeholder="Set password"><br>
+                    <hr class="mb-3">
+                    <i class="fa-solid fa-lock"></i>
+                    <input type="password" name="pass1" id="pass1" placeholder="Re-type password"><br>
+                    <hr class="mb-3">
+                    <input class="btn btn-primary" type="submit" name="submit" id="submit" value="Submit"><br>
+
+                </form>
+
+                        </div>
+
+                    </div>
+                </div>
+               
+            </div>
+        </body>
+        <td></td>
+    </html> 
